abc6fcd7408360f8e3e2f5289a34d871c09d17cc
Jón Jonker <66784287+jonjonker@users.noreply.github.com>
2020-12-02 15:35:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log('Hello Winc Academy');
+
+let name = 'Jón Jonker';
+console.log(name);
+
+let som = 4 + 4;
+console.log(som);
+
+let som2 = 26 + 33;
+console.log(som2);
+
+let som3 = 44 - 22;
+console.log(som3);
+
+let som4 = 12 * 2;
+console.log(som4);
+
+let som5 = 14 / 3;
+console.log(som5);
+
+let som6 = 11 % 33;
+console.log(som6);
+
+let age = 31;
+console.log(age);
+
+typeof 31;
+typeof "thirthyone";
+
